c743d4a35c1f39fe7b52c69862bb36dda740adfa
Dominic Heule <dominic.heule@noseryoung.com>
2023-06-07 14:08:05 +0200

    login implemented

--- src/components/pages/LoginService.ts
+++ src/components/pages/LoginService.ts
@@ -1,38 +1,19 @@
 import {AxiosInstance} from "axios";
 import { defaultAxiosInstance } from "./Api";
-import User from "./types";
+import { User } from "./types";
 
 
-export const PublisherServicePost = (api: AxiosInstance = defaultAxiosInstance) => ({
-    getPublisher: async ({email, password, accessToken}: User) => {
+export const UserServicePost = (api: AxiosInstance = defaultAxiosInstance) => ({
+    Login: async ({email, password}: User) => {
         const response = await api.post<any>(`login`, { email, password });
-        return response.data;
-    }
-});
-
-
-export const PublisherServicePut = (api: AxiosInstance = defaultAxiosInstance) => ({
-    getPublisher: async (id: any, {publisher_name, incorporation_date}: Publisher) => {
-        const response = await api.put<any>(`publisher/${id}`, { publisher_name, incorporation_date });
-        if (response && response.status === 200) {
+        if (response && response.status === 201) {
             console.log("publisher successfully updated");
         }else{
             console.log("asldfkjkj");
         }
+        return response.data;
     }
 });
 
-export const PublisherServiceDelete = (api: AxiosInstance = defaultAxiosInstance) => ({
-    getPublisher: async (id: any) => {
-        console.log("test");
-        const response = await api.delete<any>(`publisher/${id}`);
-        if (response && response.status === 200) {
-            console.log("publisher successfully deleted");
-        }else{
-            console.log("asldfkjkj");
-        }
-    }
-});
-
-export default PublisherServiceGet;
+export default UserServicePost;
 
